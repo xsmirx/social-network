3f2ba7cbb0f4e0f45c2025935b0d12f48c24f217
Ivan Smirnov <xsmirx@gmail.com>
2021-06-08 03:40:18 +0300

    51 axios and usersAPI

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import style from "./User.module.css";
+import avatar from "../../../assets/user.png";
 
 export const User = (props) => {
   return (
-    <div key={props.id} className={style.user}>
+    <div className={style.user}>
       <div className={style.avatar}>
-        <img src={props.avatarUrl} />
+        <img src={props.avatarUrl || avatar} />
       </div>
       <div className={style.title}>
         <div className={style.subtitle}>
           <div className={style.description}>
             <p className={style.fullname}>{props.fullName}</p>
-            <p className="status">{props.status}</p>
+            <p className="status">{props.status || "no status"}</p>
           </div>
           <div className={style.location}>
-            <p className="country">{props.location.country}</p>
-            <p className="city">{props.location.city}</p>
+            <p className="country">{"props.location.country"}</p>
+            <p className="city">{"props.location.city"}</p>
           </div>
         </div>
         <div className="button">
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,58 +1,24 @@
 import React from "react";
 import { User } from "./User/User";
 import style from "./Users.module.css";
+import * as axios from "axios";
 
 export const Users = (props) => {
-  // not pure function
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        followed: true,
-        avatarUrl:
-          "https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg",
-        fullName: "Inna",
-        status: "I love Ivan",
-        location: { city: "Magnitogorsk", country: "Russia" },
-      },
-      {
-        id: 2,
-        followed: false,
-        avatarUrl:
-          "https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg",
-        fullName: "Masha",
-        status: "I love Ivan",
-        location: { city: "Moskow", country: "Russia" },
-      },
-      {
-        id: 3,
-        followed: true,
-        avatarUrl:
-          "https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg",
-        fullName: "Mira",
-        status: "I love Ivan",
-        location: { city: "Minsk", country: "Belarus" },
-      },
-      {
-        id: 4,
-        followed: false,
-        avatarUrl:
-          "https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg",
-        fullName: "Nastya",
-        status: "I love Ivan",
-        location: { city: "NY", country: "USA" },
-      },
-    ]);
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        props.setUsers(response.data.items);
+      });
   }
-  //----
 
   let users = props.users.map((u) => {
     return (
       <User
-        id={u.id}
+        key={u.id}
         followed={u.followed}
-        avatarUrl={u.avatarUrl}
-        fullName={u.fullName}
+        avatarUrl={u.photos.small}
+        fullName={u.name}
         status={u.status}
         location={u.location}
         follow={props.follow}
