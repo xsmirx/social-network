ae799445aed338f3338abf7ed46a720e12a6c58c
Ivan Smirnov <xsmirx@gmail.com>
2021-06-08 04:50:35 +0300

    54. Users is class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,35 +3,68 @@ import { User } from "./User/User";
 import style from "./Users.module.css";
 import * as axios from "axios";
 
-export const Users = (props) => {
-  let getUsers = () => {
-    if (props.users.length === 0) {
+export class Users extends React.Component {
+  getUsers = () => {
+    if (this.props.users.length === 0) {
       axios
         .get("https://social-network.samuraijs.com/api/1.0/users")
         .then((response) => {
-          props.setUsers(response.data.items);
+          this.props.setUsers(response.data.items);
         });
     }
   };
-
-  let users = props.users.map((u) => {
+  render() {
     return (
-      <User
-        key={u.id}
-        followed={u.followed}
-        avatarUrl={u.photos.small}
-        fullName={u.name}
-        status={u.status}
-        location={u.location}
-        follow={props.follow}
-        unfollow={props.unfollow}
-      />
+      <div className={style.users}>
+        <button onClick={this.getUsers}>getUsers</button>
+        {this.props.users.map((u) => {
+          return (
+            <User
+              key={u.id}
+              followed={u.followed}
+              avatarUrl={u.photos.small}
+              fullName={u.name}
+              status={u.status}
+              location={u.location}
+              follow={this.props.follow}
+              unfollow={this.props.unfollow}
+            />
+          );
+        })}
+      </div>
     );
-  });
-  return (
-    <div className={style.users}>
-      <button onClick={getUsers}>getUsers</button>
-      {users}
-    </div>
-  );
-};
+  }
+}
+
+// export const Users = (props) => {
+//   let getUsers = () => {
+//     if (props.users.length === 0) {
+//       axios
+//         .get("https://social-network.samuraijs.com/api/1.0/users")
+//         .then((response) => {
+//           props.setUsers(response.data.items);
+//         });
+//     }
+//   };
+
+//   let users = props.users.map((u) => {
+//     return (
+//       <User
+//         key={u.id}
+//         followed={u.followed}
+//         avatarUrl={u.photos.small}
+//         fullName={u.name}
+//         status={u.status}
+//         location={u.location}
+//         follow={props.follow}
+//         unfollow={props.unfollow}
+//       />
+//     );
+//   });
+//   return (
+//     <div className={style.users}>
+//       <button onClick={getUsers}>getUsers</button>
+//       {users}
+//     </div>
+//   );
+// };
