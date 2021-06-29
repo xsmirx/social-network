60dac9aa701749434e18d514a8b21ebe5d5dd640
Ivan Smirnov <xsmirx@gmail.com>
2021-06-29 23:26:53 +0300

    styles for login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import style from "./Login.module.css";
 import { Redirect } from "react-router-dom";
 import { useFormik } from "formik";
-import { TextField, Checkbox, Button } from "@material-ui/core";
+import { TextField, Checkbox, Button, makeStyles } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
 import * as yup from "yup";
 
@@ -22,7 +22,16 @@ const validationSchema = yup.object().shape({
   password: yup.string().min(6).required(),
 });
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: "10px auto"
+    },
+  },
+}));
+
 const LoginForm = (props) => {
+  const classes = useStyles();
   const formik = useFormik({
     initialValues: { email: "", password: "", rememberMe: false },
     onSubmit: async (values) => {
@@ -30,46 +39,41 @@ const LoginForm = (props) => {
     },
     validationSchema: validationSchema,
   });
-  console.log(formik.isSubmitting);
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div>
-        <TextField
-          name="email"
-          label="e-mail"
-          type="email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.errors.email}
-          color="primary"
-          fullWidth
-        />
-      </div>
-      <div>
-        <TextField
-          name="password"
-          label="password"
-          type="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          error={formik.touched.password && Boolean(formik.errors.password)}
-          helperText={formik.errors.password}
-          color="primary"
-          fullWidth
-        />
-      </div>
-      <div>
-        <Checkbox
-          name="rememberMe"
-          checked={formik.values.rememberMe}
-          onChange={formik.handleChange}
-          color="primary"
-        />
-        remember me
-      </div>
+    <form className={classes.root} onSubmit={formik.handleSubmit}>
+      <TextField
+        name="email"
+        label="e-mail"
+        type="email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.email && Boolean(formik.errors.email)}
+        helperText={formik.errors.email}
+        variant="outlined"
+        color="primary"
+        fullWidth
+      />
+      <TextField
+        name="password"
+        label="password"
+        type="password"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.password && Boolean(formik.errors.password)}
+        helperText={formik.errors.password}
+        variant="outlined"
+        color="primary"
+        fullWidth
+      />
+      <Checkbox
+        name="rememberMe"
+        checked={formik.values.rememberMe}
+        onChange={formik.handleChange}
+        color="primary"
+      />
+      remember me
       <Button
         type="submit"
         variant={"contained"}
