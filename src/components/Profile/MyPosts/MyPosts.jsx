05654fe399ebe34731aa18476c51ca1dafcad0f6
Ivan Smirnov <xsmirx@gmail.com>
2021-07-05 03:05:30 +0300

    87. React.memo for MyPosts component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,11 +3,12 @@ import style from "./MyPosts.module.css";
 import { PostsContainer } from "./Posts/PostsContainer";
 import { TextAreaContainer } from "./TextArea/TextAreaContainer";
 
-export const MyPosts = () => {
+export const MyPosts = React.memo(() => {
+  console.log("render");
   return (
     <div className={style.myposts}>
       <TextAreaContainer />
       <PostsContainer />
     </div>
   );
-};
+});
